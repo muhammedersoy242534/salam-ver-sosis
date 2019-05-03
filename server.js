c79f4eeb5c1de8fd0014aff661636e1ffe5783cc
Glitch (hello-express) <none>
2019-05-03 17:54:57 +0000

    🍳🏌 Checkpoint ./komutlar/girişçıkış.js:4177304/1474 ./server.js:4177304/3501 ./komutlar/herkeserolver.js:4177304/1286

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
-// server.js
-// where your node app starts
+///////////////RESİMLİ GİRİŞ ÇIKIŞ///////////////////////////
+client.on("guildMemberAdd", async member => {
+const fs = require('fs');
+let gc = JSON.parse(fs.readFileSync("./jsonlar/gc.json", "utf8"));
+  
+  const hgK = member.guild.channels.get(gc[member.guild.id].gkanal)
+    if (!hgK) return;
+        let username = member.user.username;
+   
+            const bg = await Jimp.read("https://i.postimg.cc/LXrHDVJC/guildAdd.png");
+            const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+              setTimeout(function () {
+                    hgK.send(new Discord.Attachment("./img/" + member.id + ".png"));
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./img/" + member.id + ".png");
+              }, 10000);
+        let hgm = JSON.parse(fs.readFileSync("./jsonlar/hgm.json", "utf8"));
+    const hgmK = member.guild.channels.get(hgm[member.guild.id].gkanal)
+    var kullanici = member.tag
+    var sunucu = member.guild.name
+    hgmK.send(`${gc[member.guild.id].mesaj}`)
+    })
+client.on("guildMemberRemove", async member => {
+const fs = require('fs');
+let gc = JSON.parse(fs.readFileSync("./jsonlar/gc.json", "utf8"));
+    const hgK = member.guild.channels.get(gc[member.guild.id].gkanal)
+    if (!hgK) return;
+        let username = member.user.username;
+         
+                        const bg = await Jimp.read("https://i.postimg.cc/zGJqxvfr/guild-Remove.png");
+            const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+              setTimeout(function () {
+                    hgK.send(new Discord.Attachment("./img/" + member.id + ".png"));
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./img/" + member.id + ".png");
+              }, 10000);
+        
+    })
 
-// init project
-const express = require('express');
-const app = express();
+//CODERS KOD PAYLAŞIM VE BOT BARINDIRMA
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+//////////////////////
