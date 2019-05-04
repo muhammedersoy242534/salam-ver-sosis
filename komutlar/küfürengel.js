f94a5637d037526b93c8023672f3e48b0ab23601
Glitch (hello-express) <none>
2019-05-04 15:53:10 +0000

    🌷🍬 Checkpoint ./komutlar/küfürengel.js:4177304/2964 ./jsonlar/küfürEngelle.json:4177304/2

--- komutlar/küfürengel.js
+++ komutlar/küfürengel.js
@@ -1,34 +1,56 @@
-const db = require('quick.db')
-const Discord = require('discord.js')
-const ayarlar = require('../ayarlar.json');
+const Discord = require('discord.js');
+const fs = require('fs');
+let küfürEngel = JSON.parse(fs.readFileSync("././jsonlar/kufurEngelle.json", "utf8"));
 
-exports.run = async (bot, message, args) => {
-  if (!args[0]) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`Lütfen **aç** veya **kapat** Yazmalısın!  ${ayarlar.prefix}küfürengel aç `).setColor("RED"));
-  if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` Yetkisine Sahip Olmalısın!')
-  
-  if (args[0] == 'aç') {
-    db.set(`kufur_${message.guild.id}`, 'acik').then(i => {
-     return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Küfür Engel Başarıyla Açıldı! `Üyeleri Yasakla` Yetkisine Sahip Olanların Küfürü Engellenmicektir.').setColor("RANDOM"));
-    })
-  }
-  if (args[0] == 'kapat') {
-    db.set(`kufur_${message.guild.id}`, 'kapali').then(i => {
-     return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Küfür Filtresi Başarıyla Kapatıldı.').setColor("RANDOM"));
-    })
-  }
+var ayarlar = require('../ayarlar.json');
 
+exports.run = (client, message) => {
+
+	//if (message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`);
+
+	let args = message.content.split(' ').slice(1);
+	const secenekler = args.slice(0).join(' ');
+
+	var errembed = new Discord.RichEmbed()
+	.setColor("RANDOM")
+	.setDescription(`Yanlış Kullanım!`)
+	.addField(`Doğru Kullanım:`, `${ayarlar.prefix}küfür-engelle aç veya kapat`)
+	if(secenekler.length < 1) return message.channel.send(errembed);
+	//if(secenekler === "aç" || "kapat") return message.channel.send(errembed);
+
+	if (secenekler === "aç") {
+		message.channel.send(`Küfür Engelleme Sistemi: **açık**!`)
+		küfürEngel[message.guild.id] = {
+			küfürEngel: "acik"
+		  };
+
+		  fs.writeFile("././jsonlar/kufurEngelle.json", JSON.stringify(küfürEngel), (err) => {
+			if (err) console.log(err)
+		  });
+	};
+
+	if (secenekler === "kapat") {
+		message.channel.send(`Küfür Engelleme Sistemi: **kapalı**!`)
+		küfürEngel[message.guild.id] = {
+			küfürEngel: "kapali"
+		  };
+
+		fs.writeFile("././jsonlar/kufurEngelle.json", JSON.stringify(küfürEngel), (err) => {
+			if (err) console.log(err)
+		  });
+	};
 }
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['küfür-engel'],
-  kategori: "moderasyon",
-  permLevel: 3
-};
-
-exports.help = {
-  name: 'küfürengel',
-  description: 'Küfür Engelleme Sistemini Açar/Kapatır.',
-  usage: 'küfür-engelleme'
-};
+	exports.conf = {
+		enabled: true,
+		guildOnly: false,
+		aliases: ['küfür-engel'],
+		permLevel: `Yönetici izni gerekiyor. (${3})`
+	  };
+	  
+	  exports.help = {
+		name: 'küfür-engelle',
+    category: "ayarlar",
+		description: 'Küfür engelleme sistemini açıp kapatmanızı sağlar.',
+		usage: '>küfürengelle <aç> veya <kapat>'
+	  };
