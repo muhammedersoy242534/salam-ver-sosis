64eca621de6690b2037976a864e88f2e19d409af
Glitch (hello-express) <none>
2019-05-04 13:00:53 +0000

    ❣️🏖 Checkpoint ./server.js:2808062/2450 ./jsonlar/cogc.json:2808062/2 ./package.json:2808062/7738 ./ayarlar/kayit2.json:2808062/2 ./komutlar/kayıtkanalı.js:4177304/1 ./komutlar/kayıt-kanal-ayarla.js:4177304/1156 ./ayarlar/kayit1.json:4177304/2

--- komutlar/kayıtkanalı.js
+++ komutlar/kayıtkanalı.js
@@ -4,7 +4,7 @@ const db = require('quick.db');
 exports.run = (client, message, params, args) => {
      if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(':no_entry: Kayıt kanalı ayarlamak için `Yönetici` yetkisine sahip olman gerek.')
      let logkanali = message.mentions.channels.first();
-     if (!logkanali) return message.channel.send(':no_entry: Kayıt kanalı ayarlamak için bir kanal etiketlemeniz gerekli. `i!logayarla #kanal`')
+     if (!logkanali) return message.channel.send(':no_entry: Kayıt kanalı ayarlamak için bir kanal etiketlemeniz gerekli. `ilogayarla #kanal`')
      db.set(`membermodChannel_${message.guild.id}`, message.mentions.channels.first().id).then(i => {
         message.channel.send(`:white_check_mark: Kayıt kanalı, <#${i}> olarak ayarlandı.`)    
     })         
--- server.js
+++ server.js
@@ -95,6 +95,90 @@ if (!i) return;
 
 
 
+const log = message => {
+    console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
 
 ////////////////////MOD LOG///////////////////////
 
