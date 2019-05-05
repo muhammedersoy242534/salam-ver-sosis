019496443b2566a60c6101a425d264a6af172616
Glitch (bot-komut) <none>
2019-05-05 12:21:06 +0000

    💰🐋 Checkpoint ./server.js:2306186/3372 ./events/message.js:2306186/1649

--- server.js
+++ server.js
@@ -15,65 +15,69 @@ console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
-if (err) console.error(err);
-log(`${files.length} komut yüklenecek.`);
-files.forEach(f => {
-let props = require(`./komutlar/${f}`);
-log(`Yüklenen komut: ${props.help.name}.`);
-client.commands.set(props.help.name, props);
-props.conf.aliases.forEach(alias => {
-client.aliases.set(alias, props.help.name);
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
 });
-});
-});
+
 client.reload = command => {
-return new Promise((resolve, reject) => {
-try {
-delete require.cache[require.resolve(`./komutlar/${command}`)];
-let cmd = require(`./komutlar/${command}`);
-client.commands.delete(command);
-client.aliases.forEach((cmd, alias) => {
-if (cmd === command) client.aliases.delete(alias);
-});
-client.commands.set(command, cmd);
-cmd.conf.aliases.forEach(alias => {
-client.aliases.set(alias, cmd.help.name);
-});
-resolve();
-} catch (e){
-reject(e);
-}
-});
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
 };
+
 client.load = command => {
-return new Promise((resolve, reject) => {
-try {
-let cmd = require(`./komutlar/${command}`);
-client.commands.set(command, cmd);
-cmd.conf.aliases.forEach(alias => {
-client.aliases.set(alias, cmd.help.name);
-});
-resolve();
-} catch (e){
-reject(e);
-}
-});
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
 };
+
 client.unload = command => {
-return new Promise((resolve, reject) => {
-try {
-delete require.cache[require.resolve(`./komutlar/${command}`)];
-let cmd = require(`./komutlar/${command}`);
-client.commands.delete(command);
-client.aliases.forEach((cmd, alias) => {
-if (cmd === command) client.aliases.delete(alias);
-});
-resolve();
-} catch (e){
-reject(e);
-}
-});
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
 };
+
 /////////////////////MOD LOG////////////////////////////
 client.on('guildBanAdd', async (guild, member) => {
    const embed = new Discord.RichEmbed()
