0397dddec8cbd74dd23a78a20855b8aefc8c3b1a
Glitch (bot-komut) <none>
2019-05-05 15:05:05 +0000

    🚗🍔 Checkpoint ./komutlar/havadurumu.js:4177304/772 ./komutlar/seviye.js:4177304/31330 ./package.json:4177304/2099 ./server.js:4177304/486

--- server.js
+++ server.js
@@ -328,4 +328,24 @@ client.on("message", msg => {
     }
 });
 
+///////////////////////////SEVİYE SİSTEMİ////////////////////////////
+client.on("message", async msg => {
+  
+  if (msg.channel.type === "dm") return;
+  if(msg.author.bot) return;  
+  
+  if (msg.content.length > 7) {
+    
+    db.add(`puan_${msg.author.id + msg.guild.id}`, 3)
+};
+
+  if (db.fetch(`puan_${msg.author.id + msg.guild.id}`) > 150) {
+    
+    db.add(`seviye_${msg.author.id + msg.guild.id}`, 1)
+    
+    db.delete(`puan_${msg.author.id + msg.guild.id}`)
+    
+  };
+  
+});
 client.login(ayarlar.token);
