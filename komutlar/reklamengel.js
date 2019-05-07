6f2f8fa9a3b9763fc7121209304c74def09f16f0
Glitch (bot-komut) <none>
2019-05-07 14:39:07 +0000

    😎🐬 Checkpoint ./server.js:4177304/1417 ./komutlar/reklamengel.js:4177304/1053

---
+++ komutlar/reklamengel.js
@@ -0,0 +1,34 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json')
+var prefix = ayarlar.prefix;
+
+exports.run = async (client, message, args) => {
+  if (!args[0]) return message.channel.send('✅Aç yada Kapat yazmalısın! Örnek: p!reklamengel aç')
+  if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!')
+  
+  if (args[0] == 'aç') {
+    db.set(`reklam_${message.guild.id}`, 'Açık').then(i => {
+      message.channel.send('✅ **Reklam Engel başarıyla açıldı! Üyeleri Yasakla yetkisine sahip olanların reklamı engellenmicektir.**')
+    })
+  }
+  if (args[0] == 'kapat') {
+    db.set(`reklam_${message.guild.id}`, 'Kapalı').then(i => {
+      message.channel.send('✅ **Reklam Engel başarıyla kapatıldı! Artık herkes reklam yapabilir.**')
+    })
+  }
+
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['reklam'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'reklamengel',
+  description: 'reklamengel',
+  usage: 'reklam-engel'
+};
+   
--- server.js
+++ server.js
@@ -288,24 +288,29 @@ console.log(err);
 
 
 /////////////////////////REKLAM ENGELLEME/////////////////////////
-client.on("message", msg => { 
-if (!linkEngel[msg.guild.id]) return;
-if (linkEngel[msg.guild.id].linkEngel === "kapali") return;
-if (linkEngel[msg.guild.id].linkEngel === "acik") {
-var regex = new RegExp(/(discord.gg|http|.gg|.com|.net|.org|invite|İnstagram|Facebook|watch|Youtube|youtube|facebook|instagram)/)
-if (regex.test(msg.content)== true) {
-if (!msg.member.hasPermission("ADMINISTRATOR")) {
-msg.delete()
-msg.channel.send(`<@${msg.author.id}>`).then(message => message.delete(5000));
-var e = new Discord.RichEmbed()
-.setColor("RANDOM")
-.setAuthor("Link Engeli!")
-.setDescription(`Bu sunucuda linkler **${client.user.username}** tarafından engellenmektedir! Link atmana izin vermeyeceğim!`)
-msg.channel.send(e).then(message => message.delete(5000));
+client.on("message", async msg => {
+  db.fetch(`reklam_${msg.guild.id}`).then(i => {
+if (i == 'Açık') {
+
+    const reklam = ["discordapp", ".com", ".net", ".xyz", ".tk", "gulu", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl"];
+        if (reklam.some(word => msg.content.includes(word))) {
+          try {
+             if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+               
+
+                  return msg.reply('Reklam Tespit Edildi! ⚠').then(msg => msg.delete(3000));
+             }
+          } catch(err) {
+            console.log(err);
+          }
+        } } else if (i == 'Kapalı') {
+
 }
-}
-}
+
+})
 });
+
 ////////////////////////EVERYONE ENGELLEME///////////////////
 
 client.on("message", msg => {
