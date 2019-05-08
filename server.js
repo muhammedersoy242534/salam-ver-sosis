d735595e0215c313826fa4fe104431a8c55dae5b
Glitch (bot-komut) <none>
2019-05-08 15:49:21 +0000

    🚟💄 Checkpoint ./server.js:4177304/180

--- server.js
+++ server.js
@@ -824,7 +824,8 @@ let cogc = JSON.parse(fs.readFileSync("./jsonlar/cogc.json", "utf8"));
     if (!hgK) return;
         let username = member.user.username;
    
-            const bg = await Jimp.read("https://i.postimg.cc/LXrHDVJC/guildAdd.png");
+            const bg = await
+Jimp.read("https://i.postimg.cc/W3XJf156/12.png");
             const userimg = await Jimp.read(member.user.avatarURL);
             var font;
             if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
@@ -832,14 +833,13 @@ let cogc = JSON.parse(fs.readFileSync("./jsonlar/cogc.json", "utf8"));
             else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
             await bg.print(font, 430, 170, member.user.tag);
             await userimg.resize(362, 362);
-            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+            await bg.composite(userimg, 4, 26).write("./img/"+ member.id + ".png");
               setTimeout(function () {
                     hgK.send(new Discord.Attachment("./img/" + member.id + ".png"));
               }, 1000);
               setTimeout(function () {
                 fs.unlink("./img/" + member.id + ".png");
               }, 10000);
-  let cogc = JSON.parse(fs.readFileSync("./jsonlar/cogc.json", "utf8"));
     const hgmK = member.guild.channels.get(cogc[member.guild.id].gkanal)
     var kullanici = member.tag
     var sunucu = member.guild.name
