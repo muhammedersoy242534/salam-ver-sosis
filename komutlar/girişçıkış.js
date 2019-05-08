49abfbb4124c212af0e30dae9a404a2aa7867f36
Glitch (bot-komut) <none>
2019-05-08 14:32:17 +0000

    🎋👜 Checkpoint ./komutlar/girişçıkış.js:4177304/7226 ./server.js:4177304/5715 ./jsonlar/cogc.json:4177304/6

--- komutlar/girişçıkış.js
+++ komutlar/girişçıkış.js
@@ -1,27 +1,49 @@
-const Discord = require('discord.js');
-exports.run = (client, message, args) => { 
-    var Jimp = require("jimp");
-    const Discord = require('discord.js');
-    let img    = Jimp.read(message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL),
-    moldura = Jimp.read("https://i.imgur.com/tZ6OQWe.png");
-    Promise.all([img, moldura]).then(imgs => {
-    let moldura = imgs[0],
-        img = imgs[1];
-    moldura.resize(78, 74);    // KULLANICI FOTOĞAFI <-
-    img.resize(392, 184)   // ARKA PLAN BÜYÜKLÜĞÜ
-    img.composite(moldura, 25, 53).getBuffer(Jimp.MIME_PNG, (err, buffer) => { // X-Y-Z
-        if (!err) 
-        message.channel.send(new Discord.Attachment(buffer));        
-    });
-})};
+const Discord = require('discord.js')
+const fs = require('fs');
+var ayarlar = require('../ayarlar.json');
+let kanal = JSON.parse(fs.readFileSync("././jsonlar/cogc.json", "utf8"));
+
+exports.run = async (client, message, args) => {
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`);
+  
+  let channel = message.mentions.channels.first()
+  
+    if (!channel) {
+        const embed = new Discord.RichEmbed()
+        .setColor("RED")
+          .setTitle(`Yanlış Kullanım!`)
+          .addField(`Doğru Kullanımı`, `${ayarlar.prefix}giriş-çıkış-ayarla <#kanal>`)
+        message.channel.send({embed})
+        return
+    }
+
+    if(!kanal[message.guild.id]){
+        kanal[message.guild.id] = {
+            gkanal: channel.id
+        };
+    }
+  
+    fs.writeFile("././jsonlar/cogc.json", JSON.stringify(kanal), (err) => {
+        console.log(err)
+    })
+  
+    const embed = new Discord.RichEmbed()
+    .setDescription(`» Giriş Çıkış kanalı başarıyla ${channel} olarak ayarlandı!`)
+    .setColor("GREEN")
+    message.channel.send({embed})
+  
+
+}
+    
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
+    enabled: true,
+    guildOnly: false,
+    aliases: ['giriş-çıkış-belirle',"girişçıkış","hoşgeldin","hoşgeldin-ayarla"],
+    permLevel: 0
+}
+
 exports.help = {
-  name: 'girişçıkış',
-  description: 'Hoşgedin resmi.',
-  usage: '.hoşgeldin'
-};
+    name: 'giriş-çıkış-ayarla',
+    description: 'Giriş çıkış kanalını ayarlar.',
+    usage: 'giriş-çıkış-ayarla <#kanal>'
+}
--- server.js
+++ server.js
@@ -815,4 +815,60 @@ function play(guild, song) {
   .setColor('RANDOM'));
 }
 
+/////////////////////////RESİMLİ GİRİŞ ÇIKIŞ///////////////////////
+client.on("guildMemberAdd", async member => {
+const fs = require('fs');
+let gc = JSON.parse(fs.readFileSync("./jsonlar/cogc.json", "utf8"));
+  
+  const hgK = member.guild.channels.get(cogc[member.guild.id].gkanal)
+    if (!hgK) return;
+        let username = member.user.username;
+   
+            const bg = await Jimp.read("https://i.postimg.cc/LXrHDVJC/guildAdd.png");
+            const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+              setTimeout(function () {
+                    hgK.send(new Discord.Attachment("./img/" + member.id + ".png"));
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./img/" + member.id + ".png");
+              }, 10000);
+        let cogc = JSON.parse(fs.readFileSync("./jsonlar/cogc.json", "utf8"));
+    const hgmK = member.guild.channels.get(cogc[member.guild.id].gkanal)
+    var kullanici = member.tag
+    var sunucu = member.guild.name
+    hgmK.send(`${cogc[member.guild.id].mesaj}`)
+    })
+client.on("guildMemberRemove", async member => {
+const fs = require('fs');
+let cogc = JSON.parse(fs.readFileSync("./jsonlar/cogc.json", "utf8"));
+    const hgK = member.guild.channels.get(cogc[member.guild.id].gkanal)
+    if (!hgK) return;
+        let username = member.user.username;
+         
+                        const bg = await Jimp.read("https://i.postimg.cc/zGJqxvfr/guild-Remove.png");
+            const userimg = await Jimp.read(member.user.avatarURL);
+            var font;
+            if (member.user.tag.length < 15) font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+            else if (member.user.tag.length > 15) font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+            else font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+            await bg.print(font, 430, 170, member.user.tag);
+            await userimg.resize(362, 362);
+            await bg.composite(userimg, 43, 26).write("./img/"+ member.id + ".png");
+              setTimeout(function () {
+                    hgK.send(new Discord.Attachment("./img/" + member.id + ".png"));
+              }, 1000);
+              setTimeout(function () {
+                fs.unlink("./img/" + member.id + ".png");
+              }, 10000);
+        
+    })
+
+
 client.login(ayarlar.token);
