f06302575d50d666d8b5192501c8afa055ff3605
Glitch (bot-komut) <none>
2019-05-08 14:10:21 +0000

    😽🚏 Checkpoint ./komutlar/espri.js:4177304/2040 ./komutlar/mkasözleri.js:4177304/2488 ./komutlar/sor.js:4177304/33

--- komutlar/sor.js
+++ komutlar/sor.js
@@ -7,7 +7,7 @@ exports.run = (client, message, params) => {
 bot.setNick('Valley');
 let yazi = params.slice(0).join(' ');
  if (yazi.length < 1) return message.reply('Bir mesaj yazmalısın.');
-   message.channel.send(`🔄 Mesaj Yükleniyor... Lütfen Bekleyiniz Bu İşlem Biraz Uzun Sürebilir`).then(msg => msg.delete(10000));
+   message.channel.send(`🔄 Mesaj Yükleniyor... Lütfen Bekleyiniz.`).then(msg => msg.delete(10000));
 bot.create(function (err, session) {
     bot.ask(yazi, function (err, response) {
         console.log(response)
