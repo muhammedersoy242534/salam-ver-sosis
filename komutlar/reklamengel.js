630f16843fc8afca2f91407bb788c313a3e60648
Glitch (bot-komut) <none>
2019-05-09 01:32:15 +0000

    🐋⛲️ Checkpoint ./komutlar/reklamengel.js:4177304/2059 ./server.js:4177304/491

--- komutlar/reklamengel.js
+++ komutlar/reklamengel.js
@@ -1,32 +1,33 @@
 const db = require('quick.db')
 const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json')
+var prefix = ayarlar.prefix;
 
-exports.run = async (bot, message, args) => {
-  if (!args[0]) return message.channel.send('Hey Bu Ayarı Kullanabilmek için `aç` yada `kapat` yazmalısın!')
+exports.run = async (client, message, args) => {
+  if (!args[0]) return message.channel.send(`:x: Aç yada Kapat yazmalısın! Örnek: ${prefix}reklamengel aç`)
   if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!')
   
   if (args[0] == 'aç') {
-    db.set(`reklam_${message.guild.id}`, 'acik').then(i => {
-      message.channel.send('Reklam Engel başarıyla açıldı! `Üyeleri Yasakla` yetkisine sahip olanların reklamı engellenmicektir.')
+    db.set(`creklams_${message.guild.id}`, 'aciik').then(i => {
+      message.channel.send('✅ **Reklam Engel başarıyla açıldı! Üyeleri Yasakla yetkisine sahip olanların reklamı engellenmicektir.**')
     })
   }
   if (args[0] == 'kapat') {
-    db.set(`reklam_${message.guild.id}`, 'kapali').then(i => {
-      message.channel.send('Reklam Engel başarıyla kapatıldı! Artık herkes reklam yapabilir.')
+    db.set(`creklams_${message.guild.id}`, 'kapalii').then(i => {
+      message.channel.send('✅ **Reklam Engel başarıyla kapatıldı! Artık herkes reklam yapabilir.**')
     })
   }
 
-};
-
+}
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['advertisement','reklam'],
+  guildOnly: true,
+  aliases: ['reklam'],
   permLevel: 0
 };
 
 exports.help = {
   name: 'reklamengel',
-  description: '[Admin Komutu]',
-  usage: 'link-engel'
+  description: 'reklamengel',
+  usage: 'reklamengel'
 };
