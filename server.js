6e2846329ea970cd63695d44d08ca7ee1fae811a
Glitch (bot-komut) <none>
2019-05-09 17:39:15 +0000

    ⏳🏪 Checkpoint ./server.js:4177304/42

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const queue = new Map();
 let sayac = JSON.parse(fs.readFileSync("./ayarlar/sayac.json", "utf8")); 
 let linkEngel = JSON.parse(fs.readFileSync("./jsonlar/linkEngelle.json", "utf8"));
 require('./util/eventLoader')(client);
+var emoji = client.emojis.get("emoji id")
 var prefix = ayarlar.prefix;
 const log = message => {
 console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
