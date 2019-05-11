d93b2aebf16db9f32b86d1d1d21e9a477acd57b5
Glitch (bot-komut) <none>
2019-05-11 14:01:27 +0000

    🍤🖐 Checkpoint ./komutlar/yardım.js:4177304/3241

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
         .setTitle(`${client.user.username} - Komutlar`)
         .setDescription(`:white_small_square: | **${ayarlar.prefix}yetkili** Moderasyon Komutları.\n :white_small_square: | **${ayarlar.prefix}kullanıcı** Kullanıcıya Komutları.\n :white_small_square: |  **${ayarlar.prefix}eğlence** Eğlence Komutları.\n :white_small_square: | **${ayarlar.prefix}ekstra** Ekstra Komutları.\n :white_small_square: | **${ayarlar.prefix}müzik** Müzik Komutları.\n`)
         .setThumbnail(client.user.avatarURL)
-        .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=${botid}&sco&permissions=8) **|** [Destek Sunucusu](https://dis.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://dirdbots.org/bot/${botid}/vote) **|
+        .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=${botid}&scrmissions=8) **|** [Destek Sunucusu](https://dird.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://disdorg/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
     return message.channel.sendEmbed(embed);
   
@@ -21,7 +21,7 @@ exports.run = (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['help'],
+  aliases: ['help','h'],
   permLevel: 0,
 };
 
