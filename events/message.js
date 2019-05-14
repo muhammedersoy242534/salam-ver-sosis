11d0ca89485191150309e842bc0c9bd8f4f963e3
Glitch (bot-komut) <none>
2019-05-14 16:57:39 +0000

    ✌️🏫 Checkpoint ./events/message.js:4055901/2595

--- events/message.js
+++ events/message.js
@@ -1,4 +1,23 @@
 const ayarlar = require('../ayarlar.json');
+module.exports = message => {
+  let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
+
+};
 module.exports = async message => {
   let client = message.client;
   let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
@@ -17,5 +36,11 @@ module.exports = async message => {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
-
+  if (!client.commands.has(command)) {
+    if (client.aliases.has(command)) {
+      return false;
+    } else {
+    message.channel.send(`Komutlarımda \`\`${command}\`\` adında bir komut bulamadım!| Komut listesine bakmak için: \`\`${ayarlar.prefix}yardım\`\``)
+    }
+  }
 };
