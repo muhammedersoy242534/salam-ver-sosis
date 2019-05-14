fdf46108c2748ef7a67ff5afb5749156a667f68d
Glitch (bot-komut) <none>
2019-05-14 14:23:08 +0000

    💇☄️ Checkpoint ./komutlar/yetkili.js:4177304/2827 ./komutlar/müzik.js:4177304/2904

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -5,8 +5,10 @@ var prefix = ayarlar.prefix;
 
 exports.run = (client, message, params) => {
   const embedyardim = new Discord.RichEmbed()
+.setThumbnail("https://78.media.tumblr.com/10b366f294d47b40d857d6e47872d0dc/tumblr_ntubqoYYsF1sqwlqgo3_250.gif")
+  .setTitle(" Yetkili Komutları ")
   .setColor('RANDOM')
-  .addField("** ⚠ Yetkili Komutları ⚠ **", `.ban = İstediğiniz kişiyi sunucudan banlar. \n.kick  = İstediğiniz kişiyi kunucudan atar. \n.unban = İstediğiniz kişinin banını açar. \n.sustur = İstediğiniz kişiyi susturur. \n.link-engelle = Reklam engeller. \n.küfür-engelle = Küfür engeller. \n.otorol = Gelen herkeze rol verir.`)
+  .addField(`.ban = İstediğiniz kişiyi sunucudan banlar. \n.kick  = İstediğiniz kişiyi kunucudan atar. \n.unban = İstediğiniz kişinin banını açar. \n.sustur = İstediğiniz kişiyi susturur. \n.link-engelle = Reklam engeller. \n.küfür-engelle = Küfür engeller. \n.otorol = Gelen herkeze rol verir.`)
     message.channel.send(embedyardim);
    
 };
