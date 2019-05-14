978aa8a9003f07cfacba1e599ef08b25c5dca51e
Glitch (bot-komut) <none>
2019-05-14 15:50:07 +0000

    🏜🎍 Checkpoint ./komutlar/yetkili.js:4177304/1949

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-let botid = ('') //bu yere botun id'sini yapıştırın.
-//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
+var prefix = ayarlar.prefix;
 
-exports.run = (client, message, args) => {
-    const embed = new Discord.RichEmbed()
- .setThumbnail("https://78.media.tumblr.com/10b366f294d47b40d857d6e47872d0dc/tumblr_ntubqoYYsF1sqwlqgo3_250.gif")
+exports.run = (client, message, params) => {
+  const embed = new Discord.RichEmbed()
+  .setTitle('Yetkili Komutları ')
   .setColor('RANDOM')
-  .addField(`.ban = İstediğiniz kişiyi sunucudan banlar. \n.kick  = İstediğiniz kişiyi sunucudan atar. \nm!unban = İstediğiniz kişinin yasağını açar. \n.sustur = İstediğiniz kişiyi susturur. \n.link-engelle = Reklam engeller. \n.küfür-engelle = Küfür engeller. \n.otorol = Gelen herkeze rol verir.`)
+  .addField(`m!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nm!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nm!unban = İstediğiniz Kişinin Yasağını Açar. \nm!sustur = İstediğiniz Kişiyi Susturur. \nm!link-engelle = Anti Reklam Koruması. \nm!küfür-engelle = Anti Küfür Koruması. \nm!otorol = Gelen Herkeze Rol.`)
     message.channel.send(embed);
    
 };
