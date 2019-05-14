ca5d0cb52c3c4f98a3cb7fa8f7149d0d2c84d0c3
Glitch (bot-komut) <none>
2019-05-14 16:34:05 +0000

    🏋🦃 Checkpoint ./komutlar/yetkili.js:4177304/2329

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -1,39 +1,25 @@
-
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-let botid = ('569833327238971394') 
+var prefix = ayarlar.prefix;
 
-exports.run = (client, message, args) => {
-    const embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setAuthor(`${client.user.username} Yetkili Komutları`)
-    .addField('s!ban','oyuncuları banlar') 
-    .addField('s!whitelist-blacklist', 'oyuncuları siyah listeye yada Beyaz listeye alır')
-    .addField('s!bot-eklet', 'adminler bu komut ile bot ekleyebilir')
-    .addField('s!davetsüre', 'süreli davet')
-    .addField('s!sustur-rol-ayarla', 'o roldeki kişi susturulur')
-    .addField('s!dil-değiştir', 'TADİLATTA :)')
-    .addField('s!onayla', 'botu sunucuda onaylar')
-    .addField('s!yetkililer', 'aktif yetkilileri gösterir ')
-    .addField('s!uyar', 'uyarı atar')
-      .addField('s!mesajat', 'Admin özel')
-    .addField('s!kanal-bilgi', 'kanal hakkında bilgi verir')
-    .addField('s!uyarı-kaldır', 'oyuncuların uyarısını kaldırır')
-    .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/3KeJ8qj) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
-    message.channel.sendEmbed(embed);
-
+exports.run = (client, message, params) => {
+  const embedyardim = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .addField("** ⚠ Sunucu Yetkilisi Komutları ⚠ **", `m!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nm!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nm!unban = İstediğiniz Kişinin Yasağını Açar. \nm!sustur = İstediğiniz Kişiyi Susturur. \nm!link-engelle = Anti Reklam Koruması. \nm!küfür-engelle = Anti Küfür Koruması. \nm!otorol = Gelen Herkeze Rol.`)
+    message.channel.send(embedyardim);
+   
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yetkili'],
-  permLevel: 0,
+  aliases: ['yetkilik'],
+  permLevel: 0
 };
 
 exports.help = {
   name: 'yetkili',
-  description: '',
-  usage: 'yetkili'
+  description: 'Sunucu Yetkilisi Komutlarını Gösterir',
+  usage: 'yetkili [komut]'
 };
-  
