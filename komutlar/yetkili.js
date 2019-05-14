ce0945786f418911aeb209736233296e8ecde821
Glitch (bot-komut) <none>
2019-05-14 15:18:06 +0000

    🕷🍮 Checkpoint ./komutlar/yetkili.js:4177304/2579

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -1,14 +1,11 @@
 const Discord = require('discord.js');
 
-let botid = ('') //bu yere botun id'sini yapıştırın.
-//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
-
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setAuthor(`${client.user.username} Yetkili Komutları`)
-    .addField(`')
-    
+.setThumbnail("https://78.media.tumblr.com/10b366f294d47b40d857d6e47872d0dc/tumblr_ntubqoYYsF1sqwlqgo3_250.gif")
+  .setTitle(" Yetkili Komutları")
+  .setColor('RANDOM')
+  .addField(`m!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nm!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nm!unban = İstediğiniz Kişinin Yasağını Açar. \nm!sustur = İstediğiniz Kişiyi Susturur. \nm!link-engelle = Anti Reklam Koruması. \nm!küfür-engelle = Anti Küfür Koruması. \nm!otorol = Gelen Herkeze Rol.`)
     message.channel.sendEmbed(embed);
 
 };
