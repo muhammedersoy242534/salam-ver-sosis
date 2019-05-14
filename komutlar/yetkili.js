6388afa7adbe7dbad4678a7c375c525c2054ca7f
Glitch (bot-komut) <none>
2019-05-14 15:40:06 +0000

    🚄🏃 Checkpoint ./komutlar/yetkili.js:4177304/917

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -5,19 +5,10 @@ let botid = ('') //bu yere botun id'sini yapıştırın.
 
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setAuthor(`${client.user.username} Yetkili Komutları`)
-    .addField('prefix+komut', 'açıklama/kullanım amacı')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
-    
-    
-    
-
-
-exports.run = (client, message, params) => {
-  const embedyardim = new Discord.RichEmbed()
+ .setThumbnail("https://78.media.tumblr.com/10b366f294d47b40d857d6e47872d0dc/tumblr_ntubqoYYsF1sqwlqgo3_250.gif")
   .setColor('RANDOM')
-  .addField("** ⚠ Sunucu Yetkilisi Komutları ⚠ **", `m!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nm!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nm!unban = İstediğiniz Kişinin Yasağını Açar. \nm!sustur = İstediğiniz Kişiyi Susturur. \nm!link-engelle = Anti Reklam Koruması. \nm!küfür-engelle = Anti Küfür Koruması. \nm!otorol = Gelen Herkeze Rol.`)
-    message.channel.send(embedyardim);
+  .addField(`.ban = İstediğiniz kişiyi sunucudan banlar. \n.kick  = İstediğiniz kişiyi sunucudan atar. \nm!unban = İstediğiniz kişinin yasağını açar. \n.sustur = İstediğiniz kişiyi susturur. \n.link-engelle = Reklam engeller. \n.küfür-engelle = Küfür engeller. \n.otorol = Gelen herkeze rol verir.`)
+    message.channel.send(embed);
    
 };
 
@@ -32,46 +23,4 @@ exports.help = {
   name: 'yetkili',
   description: 'Sunucu Yetkilisi Komutlarını Gösterir',
   usage: 'yetkili [komut]'
-};
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    message.channel.sendEmbed(embed);
-
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0,
-};
-
-exports.help = {
-  name: 'yetkili',
-  description: '',
-  usage: ''
 };
