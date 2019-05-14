9373a89161773721621d2fee39b55ba6748c8e52
Glitch (bot-komut) <none>
2019-05-14 16:23:06 +0000

    🍳💴 Checkpoint ./komutlar/yetkili.js:4055901/2324:4177304/24 ./events/guildMemberAdd.js:4055901/10333 ./komutlar/sayaç-ayarla.js:4055901/7262 ./komutlar/sayaçsıfırla.js:4055901/2502 ./server.js:4055901/41260

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**!');
+    member.sendMessage('Hg ');
     member.guild.defaultChannel.send('hg '+username+'');
 };
