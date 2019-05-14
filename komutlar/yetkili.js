d547fd4593908172496281a7ba3e749c02d28668
Glitch (bot-komut) <none>
2019-05-14 15:07:07 +0000

    🏈🏍 Checkpoint ./komutlar/yetkili.js:4177304/2519

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -1,27 +1,28 @@
 const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
 
-var prefix = ayarlar.prefix;
+let botid = ('') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
 
-exports.run = (client, message, params) => {
-const embedyardim = new Discord.RichEmbed()
-.setThumbnail("https://78.media.tumblr.com/10b366f294d47b40d857d6e47872d0dc/tumblr_ntubqoYYsF1sqwlqgo3_250.gif")
-  .setTitle(" Yetkili Komutları ")
-  .setColor('RANDOM')
-  .addField(`m!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nm!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nm!unban = İstediğiniz Kişinin Yasağını Açar. \nm!sustur = İstediğiniz Kişiyi Susturur. \nm!link-engelle = Anti Reklam Koruması. \nm!küfür-engelle = Anti Küfür Koruması. \nm!otorol = Gelen Herkeze Rol.`)
-    message.channel.send(embedyardim);
-   
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(`${client.user.username} Yetkili Komutları`)
+    .addField(`')
+    
+    message.channel.sendEmbed(embed);
+
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yetkilik'],
-  permLevel: 0
+  aliases: [],
+  permLevel: 0,
 };
 
 exports.help = {
   name: 'yetkili',
-  description: 'Sunucu Yetkilisi Komutlarını Gösterir',
-  usage: 'yetkili [komut]'
+  description: '',
+  usage: ''
 };
+   
