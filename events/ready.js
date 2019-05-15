3804505f7e70079638f071579b8561b9ab06b863
Glitch (bot-komut) <none>
2019-05-15 18:17:01 +0000

    👀🙈 Checkpoint ./events/ready.js:4055901/3529

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+
+Save  New  Duplicate & Edit  Just Text
 const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
@@ -8,15 +10,31 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("idle");
+  client.user.setStatus("online");
+  client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
+
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
 var oyun = [
-  " ❯ .yardım"," ❯ 💪7/24 AKTİF!"," ❯ 🌍202 Kullanıcı"," ❯ İyi Oyunlar" 
-         ];
+      prefix + "yardım|Beni Sunucuna Ekle⚡️",
+    ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv");
-        }, 2 * 2500);
+        client.user.setGame(oyun[random], "https://www.twitch.tv/emirhansaracyt" );
+        }, 2 * 2550);
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 }
