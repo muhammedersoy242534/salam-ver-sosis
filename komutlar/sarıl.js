0d3c20c0ee5434e5f8a8f512554a300900829e8d
Glitch (bot-komut) <none>
2019-05-16 14:23:34 +0000

    🏌🎽 Checkpoint ./server.js:4055901/116 ./komutlar/afk.js:3800959/114 ./komutlar/ailemiz.js:4055901/811 ./komutlar/alıntı.js:4055901/1543 ./package.json:4055901/2636 ./komutlar/sarıl.js:4055901/1620

---
+++ komutlar/sarıl.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+
+const ayarlar = require("../ayarlar.json")
+
+var prefix = ayarlar.prefix;
+
+exports.run = async (client, message, args) => {
+message.delete()
+  
+  let user = message.mentions.users.first();
+  if(!user) {
+    
+const embed = new Discord.RichEmbed()
+.setTitle(`${message.guild.name}  - Sevenleri Kavuşturdu !`)
+.setColor('RANDOM')
+.setDescription(`**\n:x: Sarılacağın birisini etiketlemelisin !** \n\n** Doğru Kullanım :: ** \`\`\`${ayarlar.prefix}sarıl <@kullanıcı>\`\`\` `)
+.setTimestamp()
+.setThumbnail('https://thumbs.gfycat.com/ArtisticClearcutGreyhounddog-size_restricted.gif')
+.setFooter(`${message.author.username} | Tarafından Kontrol Ediliyor..`, message.author.avatarURL )
+return  message.channel.send(embed);
+
+  }
+
+  
+var request = require('request');
+request('https://simsekapi.cf/saril', function (error, response, body) {
+    if (error) return console.log('Hata:', error);
+    else if (!error) {
+        var veri = JSON.parse(body);
+        
+
+const embed = new Discord.RichEmbed()
+.setTitle(`${message.guild.name} - Sevenleri Kavuşturdu !`)
+.setColor('RANDOM')
+.setDescription(`<@${message.author.id}> | ${user} Adlı Kullanıcıya Sarıldı. `)
+.setImage(`${veri.saril}`)
+.setTimestamp()
+.setFooter(`${message.author.username} | Tarafından Kontrol Ediliyor..`, message.author.avatarURL )
+return  message.channel.send(embed);
+   
+    }
+});
+
+} 
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['anime','sarıl'],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: 'sarıl',
+  description: 'Arkadaşına Sarılırsın.',
+  usage: 'sarıl <@kullanıcı>'
+};
--- server.js
+++ server.js
@@ -695,34 +695,6 @@ client.on("guildMemberAdd", async member => {
 
 client.login(ayarlar.token);
 
-Save New Duplicate & Edit Just Text Twitter
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
 client.on('message', async message => {
   
   let prefix = await db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
