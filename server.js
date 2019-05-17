095b90422dea60b1bc1f5e006ea0938c38da8f2e
Glitch (bot-komut) <none>
2019-05-17 14:26:21 +0000

    ⏲🐥 Checkpoint ./server.js:4055901/289

--- server.js
+++ server.js
@@ -722,3 +722,12 @@ client.on('message', async message => {
     }
   }
 });
+client.on('message', async msg => {
+    if (msg.content.toLowerCase() === prefix + "disko") {
+   if (msg.channel.type === "dm") return;
+  const rol = 'Disko' // Rol ismi buraya
+  setInterval(() => {
+      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
+      }, 350);
+  }
+});
