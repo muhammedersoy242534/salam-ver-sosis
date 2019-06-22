4101a87cc6b0277f9dc7a3176848abe7795ee216
Glitch (bot-komut) <none>
2019-06-22 15:14:06 +0000

    🐧🐕 Checkpoint ./komutlar/yardım.js:4055901/145

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
- message.channel.send('remix,alıntı,snipe,8ball,anket,ara155,ailemiz')
+ message.channel.send('remix,notlar,notbilgi,notal,notsil,alıntı,snipe,8ball,ascii,atatürk,capsengel,anket,atamınsözleri,ara155,ailemiz,deyim,düello,emoji,emojiid,espri,eval,image,çık,konuş,saat,trump,us,sarıl')
 }
 exports.conf = {
 enabled: true,
