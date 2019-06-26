a80443bfa577fc34e7f20f3208ddff5db447f9de
Glitch (bot-komut) <none>
2019-06-26 15:29:04 +0000

    🏘🐴 Checkpoint ./komutlar/trumpdüşüncesi.js:4055901/696

---
+++ komutlar/trumpdüşüncesi.js
@@ -0,0 +1,25 @@
+const snek = require('snekfetch');
+const Discord = require('discord.js');
+const api = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
+
+module.exports.run = (bot, message, args) => {
+    snek.get(api).then(r => {
+        let embed = new Discord.RichEmbed()
+        .setTitle('Trump düşünce jeneratörü bir şey buldu!')
+        .setDescription(r.body.message)
+        .setColor('RANDOM')
+        message.channel.send(embed)
+    })
+} 
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+ aliases: ['trumpnotlari', 'trumpnotları', 'trump-notlari'],
+ permLevel: 0
+}
+
+exports.help = {
+ name: 'trumpdüşüncesi',
+ description: "Turp'un düşüncesini öğrenirsiniz.",
+ usage: 'trumpdüşüncesi'
+};
