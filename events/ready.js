d8aee7b4ba919760e82de9c6bca0e939c9608803
Glitch (bot-komut) <none>
2019-06-28 12:26:19 +0000

    🕴🍅 Checkpoint ./komutlar/yardım.js:4055901/2222 ./ayarlar.json:4055901/252 ./komutlar/remix.js:4055901/8 ./events/ready.js:4055901/1414

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,24 @@
+const chalk = require('chalk');
+const moment = require('moment');
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-module.exports = async client => {
-  client.appInfo = await client.fetchApplication();
-    setInterval( async () => {
-      client.appInfo = await client.fetchApplication();
-    }, 60000);
-    
-    require("../util/dashboard.js")(client);
-};
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+   var oyun = [
+        ".remix .yardım",
+        
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/erwin");
+        }, 2 * 2500);
+}
+
+
---
+++ komutlar/remix.js
@@ -0,0 +1,23 @@
+
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+  
+    
+let komutlar = client.commands.filter(a => a.conf.kategori === "özel").map(x => `${x.help.name}: ${x.help.description}`).join("\n")
+message.channel.send(`${komutlar}`, {split: true, 'code': 'html'})
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['yardım'],
+  permLevel: 0,
+    kategori: "özel",
+ 
+};
+
+exports.help = {
+  name: 'remix',
+  description: '',
+  usage: 'yazdır <@kullanıcı> <yazı>'
+};
