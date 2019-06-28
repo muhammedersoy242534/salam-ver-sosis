7858db238f066ff749bd6120577f95958d8bb0c3
Glitch (bot-komut) <none>
2019-06-28 05:21:20 +0000

    🍈🐒 Checkpoint ./config.js:4055901/182

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  "token": "NTkxOTUxNTYzMDIzNzEyMjU3.XRSFgw.g0SqzXGmddPzVveqvRV35InOc34",
+  "token": "NTgyNjEwNTkzNzIzMzE4MzI2.XRWhsw.d2A-tntnDidHgMOZ4CIk4IsCSPg",
 
   "dashboard" : {
-    "oauthSecret": "j4li80nlyv3VlAmQjcZMlul8xnXxWx_b", // This is the `client` secret in your bot application page.
+    "oauthSecret": "jRFIMJJ-lgb0ukwdvGSw6a8ou33i1gXs", // This is the `client` secret in your bot application page.
     "callbackURL": "https://saitokontrol2.glitch.me/callback",
     "sessionSecret": "super-secret-session-thing",
     "domain": "https://saitokontrol2.glitch.me",
