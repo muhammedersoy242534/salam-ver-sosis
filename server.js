bd4b0648372f4376732b27a65090166429bda287
Glitch (bot-komut) <none>
2019-07-02 11:06:44 +0000

    💃💎 Checkpoint ./server.js:4055901/381 ./package.json:4055901/3485 ./ayarlar.json:4055901/3

--- server.js
+++ server.js
@@ -811,3 +811,14 @@ client.on('ready', msg => {
 
   },1 * 10000);
 });
+const DBL = require("dblapi.js");
+const dbl = new DBL('dbl client id', client);
+
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+})
