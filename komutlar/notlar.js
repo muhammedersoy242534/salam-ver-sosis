aa28addf95c16364496f95d081c2dd5b27d7dc38
Glitch (saitokontrol) <none>
2019-07-05 09:13:47 +0000

    🌖💐 Checkpoint ./komutlar/notlar.js:4055901/1082

--- komutlar/notlar.js
+++ komutlar/notlar.js
@@ -3,23 +3,23 @@ const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
 
-db.fetch(`notlar_${message.author.id}`).then(yazı => {
-if(!yazı) return message.channel.send('<a:iptal:59013677715
-  message.channel.sendEmbed(new Discord.RichEmbed().setDescription(`n**${annn}**`).setColor("GREY"));
+var yazı = db.fetch(`notlar_${message.author.id}`)
+if(!yazı) return message.channel.send(':x: | Hiç notun yok')
+var annn = yazı.join(',\n')
+message.channel.send(`Notların: \n**${annn}**`)
   
-})}
+}
 
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: [],
-  permLevel: 0,
-    kategori: "ekstra",
+  aliases: ['not'],
+  permLevel: 0
 };
 
 exports.help = {
   name: 'notlar',
-  description: 'notlarınız',
+  description: 'Ritar',
   usage: 'Izinsiz Paylaşmayın Aq'
-}
+};
