fb9d61a06a9458f9efd9e0349dbc5d5907b9f1cb
Glitch (bot-komut) <none>
2019-07-05 05:55:39 +0000

    🚧🍘 Checkpoint ./config.js:4055901/2

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const config = {
 
   "dashboard" : {
     "oauthSecret": "s4YTFQZJ3lnBZCrVjPPvxuC4scBos0LA", // This is the `client` secret in your bot application page.
-    "callbackURL": "https://saitokontrol2.glitch.me/callback",
+    "callbackURL": "https://saitokontrol.glitch.me/callback",
     "sessionSecret": "super-secret-session-thing",
-    "domain": "https://saitokontrol2.glitch.me",
+    "domain": "https://saitokontrol.glitch.me",
     "port": 8000
   },
 };
