0748b34afcd73b29b2e92e12b6ebbdaa617a137c
Glitch (saitokontrol) <none>
2019-07-07 15:27:31 +0000

    🍮🚏 Checkpoint ./ayarlar.json:4055901/118 ./config.js:4055901/118

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  "token": "NTk0MTA2NjAzMDU4MjMzMzQ1.XSH3ZQ.Xadmyi2q4eMZaGjkzdTT62wva1E",
+  "token": "NTk0MTA2NjAzMDU4MjMzMzQ1.XSIOUw.w7b8F-Tbo1JnuRZztZDfp6j73Y0",
 
   "dashboard" : {
     "oauthSecret": "s4YTFQZJ3lnBZCrVjPPvxuC4scBos0LA", // This is the `client` secret in your bot application page.
