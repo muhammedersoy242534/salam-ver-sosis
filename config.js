e857418d6b9c842e3d8d9d3d42b92d3311f247e7
Glitch (saitokontrol) <none>
2020-02-09 08:39:06 +0000

    🏣👍 Checkpoint ./config.js:6564720/64

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   "token": ayarlar.token,
 
   "dashboard" : {
-    "oauthSecret": "XBx8HtswD6B-wmJAETvRx3vEYIJUepoF", // This is the `client` secret in your bot application page.
+    "oauthSecret": "ZXyvpZCrXO4oC8jKdTf02DrAkXAlzSgP", // This is the `client` secret in your bot application page.
     "callbackURL": "https://fretch.glitch.me/callback",
     "sessionSecret": "super-secret-session-thing",
     "domain": "https://fretch.glitch.me",
