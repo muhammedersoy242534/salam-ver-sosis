a239a3f369743aba135d4ce6c17a4efabd581128
Glitch (fretch) <none>
2020-02-09 11:41:14 +0000

    🍖🚜 Checkpoint ./events/ready.js:6564720/1

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} İsmi İle Giriş Yapıldı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Codare Ready.js`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: codare`);
-  client.user.setStatus("LISTENIG");
+  client.user.setStatus("LISTENING");
   client.user.setGame(`${prefix}yardım + ${client.guilds.size} Sunucu + ${client.users.size} Kullanıcı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
