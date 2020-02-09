0a81812849ddcd016ad4b51c5bce3d77582ddc5d
Glitch (fretch) <none>
2020-02-09 11:39:06 +0000

    🚌🍍 Checkpoint ./events/ready.js:6564720/1416

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,18 @@
+const chalk = require('chalk');
+const moment = require('moment');
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-module.exports = async client => {
-  client.appInfo = await client.fetchApplication();
-    setInterval( async () => {
-      client.appInfo = await client.fetchApplication();
-    }, 60000);
-    
-    require("../util/dashboard.js")(client);
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} İsmi İle Giriş Yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Codare Ready.js`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: codare`);
+  client.user.setStatus("LISTENIG");
+  client.user.setGame(`${prefix}yardım + ${client.guilds.size} Sunucu + ${client.users.size} Kullanıcı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
+ 
