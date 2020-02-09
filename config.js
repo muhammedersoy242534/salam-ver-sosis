6e5436be10fccbc14f33e75f62f781d554cdefde
Glitch (adlbot) <none>
2020-02-09 12:03:09 +0000

    🖇🍯 Checkpoint ./dashboard/templates/dashboard.ejs:6564720/42 ./config.js:6564720/88 ./dashboard/templates/admin.ejs:6564720/23

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const config = {
 
   "dashboard" : {
     "oauthSecret": "ZXyvpZCrXO4oC8jKdTf02DrAkXAlzSgP", // This is the `client` secret in your bot application page.
-    "callbackURL": "https://fretch.glitch.me/callback",
+    "callbackURL": "https://adlbot.glitch.me/callback",
     "sessionSecret": "super-secret-session-thing",
-    "domain": "https://fretch.glitch.me",
+    "domain": "https://adlbot.glitch.me",
     "port": 8000
   },
 };
