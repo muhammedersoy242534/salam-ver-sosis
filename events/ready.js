d6bac0a17ca892480260cf5b82b2f554ccb11eda
Glitch (adlbot) <none>
2020-02-09 11:51:07 +0000

    🌠🚵 Checkpoint ./events/ready.js:6564720/5408

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,14 @@
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
 
 module.exports = async client => {
   client.appInfo = await client.fetchApplication();
     setInterval( async () => {
       client.appInfo = await client.fetchApplication();
     }, 60000);
-    
+  client.user.setStatus("LISTENING");
+  client.user.setGame(`${prefix}yardım + ${client.guilds.size} Sunucu + ${client.users.size} Kullanıcı!`);  
     require("../util/dashboard.js")(client);
 };
