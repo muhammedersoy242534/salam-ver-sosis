b7454cb151d5db23edff08c65197754cc07cb07c
Glitch (adlbot) <none>
2020-02-10 14:00:31 +0000

    🍮💶 Checkpoint ./server.js:6564720/24 ./package.json:6564720/120

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir('./komutlar/', (err, files) => {
 if (err) console.error(err);
-log(`${files.length} komut yüklenecek.`);
+log(`[ADLMedia] ${files.length} komut yüklenecek.`);
 files.forEach(f => {
 let props = require(`./komutlar/${f}`);
-log(`Yüklenen komut: ${props.help.name}.`);
+log(`[ADLMedia] Yüklenen komut: ${props.help.name}.`);
 client.commands.set(props.help.name, props);
 props.conf.aliases.forEach(alias => {
 client.aliases.set(alias, props.help.name);
