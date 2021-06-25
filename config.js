6850907a7cd2278e9d4e77334af959b0db7caeb2
Glitch (adlbot) <none>
2021-06-25 14:06:29 +0000

    💽🌴 Checkpoint ./ayarlar.json:19055815/180 ./config.js:19055815/32

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   "token": ayarlar.token,
 
   "dashboard" : {
-    "oauthSecret": "", // Projenizin ana sayfasında client secret var onu yapışstır.
+    "oauthSecret": "ocJtqQB4fcD2cUyW-ursal57T52o72Oh", // Projenizin ana sayfasında client secret var onu yapışstır.
     "callbackURL": "https://adlbot.glitch.me/callback",//proje adınıza göre düzenledikten sonra Bunları uygula: https://i.hizliresim.com/GGQL4Z.jpg | https://i.hizliresim.com/ZngGnG.jpg
     "sessionSecret": "super-secret-session-thing",// buraya elleme yukarısı önemli
     "domain": "https://adlbot.glitch.me",//burasıda proje ismine göre
